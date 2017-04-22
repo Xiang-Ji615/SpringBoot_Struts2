6c69dee306b8c15b6640cfa9be43d3adeb99d049
XiangJi <XiangJi@DESKTOP-U71EK9H>
2017-04-22 20:35:20 +1000

    Update on 20170422

---
+++ SpringBoot_Struts2/src/main/java/spring/boot/tst/action/TestAction.java
@@ -0,0 +1,15 @@
+package main.java.spring.boot.tst.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
+
+	
+	
+}
